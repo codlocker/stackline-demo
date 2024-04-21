e7d1c9650bf2d9acb0bcbc2164fd9b7ee8155fe8
codlocker <isahoo1@asu.edu>
2024-04-21 01:27:09 -0700

    Product Info CSS

--- src/components/ProductInfo/product-info-component.tsx
+++ src/components/ProductInfo/product-info-component.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import { useAppSelector } from "../../types/hooks";
 import { Card, Button } from "react-bootstrap";
 import { RootState } from "../../store";
+import "./product-info.scss";
+
 const ProductInfoComponent = () => {
     const productName: string = useAppSelector((state: RootState) => state.product.name);
     const productImage: string = useAppSelector((state: RootState) => state.product.image);
     const productTags: string[] = useAppSelector((state: RootState) => state.product.tags);
+    const productDescription: string = useAppSelector((state: RootState) => state.product.description);
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card className="px-2 mx-2">
             <Card.Img variant="top" src={ productImage } />
             <Card.Body>
                 <Card.Title>{ productName }</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+                { productDescription }
                 </Card.Text>
                 <div>
                     {
                         productTags.map((item, _) => (
-                            <Button variant="outline-dark" className="tag" size="sm">{item}</Button>
+                            <Button variant="outline-dark" className="productTag" size="sm">{item}</Button>
                         ))
                     }
                 </div>
